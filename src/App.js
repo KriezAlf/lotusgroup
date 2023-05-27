fbbc9c04f269b0bd92ee8ce92c090462ed5c121d
KriezAlf <ignatius.steven03@gmail.com>
2023-05-28 01:44:32 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,29 +1,63 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes, NavLink} from 'react-router-dom'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom'
 import Results from './pages/Results';
 import Home from './pages/Home';
 import './index.css';
 import { Heading } from '@chakra-ui/react';
-
+import SingleView from './pages/SingleView';
+import { SearchContext } from './context/search';
 
 function App() {
+
+  const [animeData, setAnimeData] = useState([])
+  const [singleData, setSingleData] = useState({})
+  const [animeList, setAnimeList] = useState([]);
+  const [topAnime, setTopAnime] = useState([]);
+
+  const setData = (data) => {
+    setAnimeData(data);
+  };
+
+  const setSingle = (data) => {
+    setSingleData(data);
+  };
+
+  const getTopAnime = async () => {
+    const data = await fetch(
+      `https://api.jikan.moe/v4/top/anime/1/bypopularity`
+    ).then((response) => response.json());
+
+    setTopAnime(data.top);
+  };
+
+  const search = async (searchTerm) => {
+    return fetch(`https://api.jikan.moe/v4/anime?q=${searchTerm}&sfw=true`)
+      .then((response) => response.json());
+  };
+  
+
+
+
+
   return (
     <div>
-      <a>hellow</a>
-      <Router>
-        <Heading>
-          <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="results">Results</NavLink>
-          </nav>
-        </Heading>
-        <main>
-          <Routes>
-            <Route index element={<Home />}/>
-            <Route path="results" element={<Results />}/>
-          </Routes>
-        </main>
-      </Router>
+      <SearchContext.Provider value={{search, animeData, setData, singleData, setSingle }}>
+        <Router>
+          <Heading>
+            <nav>
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="results">Results</NavLink>
+            </nav>
+          </Heading>
+          <main>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="results" element={<Results />} />
+              <Route path="single" element={<SingleView />} />
+            </Routes>
+          </main>
+        </Router>
+      </SearchContext.Provider>
     </div>
   );
 }
--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,9 +1,19 @@
 import { Heading } from "@chakra-ui/react"
+import React, {useContext, useEffect} from 'react'
+import { SearchContext } from '../context/search'
 
-export default function Results(){
+const Results=()=>{
+    const search = useContext(SearchContext);
+    useEffect(()=>{
+        search.search('Boruto').then((data)=>{
+            console.log(data);
+        });
+    }, [search]);
   return (
     <div className="results">
         <Heading my="30px" p="10px">Results</Heading>
         </div>
   )
 }
+
+export default Results;
