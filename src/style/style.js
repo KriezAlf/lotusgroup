791ca8832d3d54ccfd2931d1108cb55859a35c62
CAROLINE <128341432+Roilinn@users.noreply.github.com>
2023-05-30 01:56:41 +0000

    Update 6 files

--- src/style/style.js
+++ src/style/style.js
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import { Link as Box } from '@chakra-ui/react';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Link as Box } from "@chakra-ui/react";
 
 export const StyledDisplay = styled(Box)`
   display: flex;
 
+  background-color: #eeecf1;
   .display-anime {
     margin-top: 1rem;
     padding-top: 2rem;
@@ -16,7 +17,6 @@ export const StyledDisplay = styled(Box)`
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
     grid-gap: 2rem;
     grid-auto-rows: 500px;
-    background-color: #fff;
     border-top: 5px solid #bbbbbb;
   }
 `;
@@ -50,227 +50,236 @@ export const StyledCardLink = styled(Link)`
   }
 `;
 export const SidebarStyled = styled.div`
-margin-top: 1rem;
-background-color: #fff;
-border-top: 5px solid #bbbbbb;
-padding-right: 5rem;
-padding-left: 5rem;
-padding-top: 1rem;
+  margin-top: 1rem;
+  background-color: #fff;
+  border-top: 5px solid #bbbbbb;
+  padding-right: 5rem;
+  padding-left: 5rem;
+  padding-top: 1rem;
 
-.anime{
+  background-color: #eeecf1;
+
+  .anime {
     display: flex;
     flex-direction: column;
     width: 150px;
-    img{
-        width: 100%;
-        border-radius: 5px;
-        border: 5px solid #bbbbbb;
-        &:focus,
-  &:hover {
-    outline: none;
-    box-shadow: 0 0 5px 2px #27ae60;
+    img {
+      width: 100%;
+      border-radius: 5px;
+      border: 5px solid #bbbbbb;
+      &:focus,
+      &:hover {
+        outline: none;
+        box-shadow: 0 0 5px 2px #27ae60;
+      }
+    }
+    a {
+      margin-top: 1rem;
+      display: flex;
+      flex-direction: column;
+      gap: 0.4rem;
+      color: #444444;
+      h4 {
+        font-size: 1.1rem;
+      }
     }
   }
-    a{
-        margin-top: 1rem;
-        display: flex;
-        flex-direction: column;
-        gap: .4rem;
-        color: #444444;
-        h4{
-            font-size: 1.1rem;
-        }
-    }
-}
 `;
 
 export const AnimeItemStyled = styled.div`
-padding: 3rem 2rem;
+  padding: 3rem 2rem;
 
-.back {
-  position: absolute;
-  top: 2rem;
-  left: 2rem;
+  background-color: #eeecf1;
 
-  a {
-    font-weight: 600;
-    text-decoration: none;
-    color: #bbbbbb;
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
-  }
-}
+  .back {
+    position: absolute;
+    top: 2rem;
+    left: 2rem;
 
-.title {
-  display: inline-block;
-  margin: 3rem 0;
-  font-size: 2rem;
-  cursor: pointer;
-  background: linear-gradient(#e66465, #9198e5);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-
-.description {
-  margin-top: 2rem;
-  color: #777777;
-  line-height: 1.5rem;
-
-  button {
-    background-color: transparent;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    font-size: 1.2rem;
-    color: #27ae60;
-    font-weight: 600;
-  }
-}
-
-.trailer-con {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  iframe {
-    outline: none;
-    border: 5px solid #e5e7eb;
-    padding: 1.5rem;
-    border-radius: 10px;
-    background-color: #fff;
-  }
-}
-
-.details {
-  background-color: #fff;
-  border-radius: 20px;
-  padding: 2rem;
-  border: 5px solid #e5e7eb;
-
-  .detail {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-
-    img {
-      border-radius: 7px;
-    }
-  }
-
-  .anime-details {
-    margin-left: 1rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-
-    p {
-      display: flex;
-      gap: 1rem;
-    }
-
-    p span:first-child {
+    a {
       font-weight: 600;
-      color: #454e56;
+      text-decoration: none;
+      color: #bbbbbb;
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
     }
   }
-}
 
-.characters {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  grid-gap: 2rem;
-  background-color: #fff;
-  padding: 2rem;
-  border-radius: 20px;
-  border: 5px solid #e5e7eb;
+  .title {
+    display: inline-block;
+    margin: 3rem 0;
+    font-size: 2rem;
+    cursor: pointer;
+    background: linear-gradient(#e66465, #9198e5);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
 
-  .character {
-    padding: 0.4rem 0.6rem;
-    border-radius: 7px;
-    background-color: #ededed;
+    background-color: #eeecf1;
+  }
+
+  .description {
+    margin-top: 2rem;
+    color: #777777;
+    line-height: 1.5rem;
+
+    button {
+      background-color: transparent;
+      border: none;
+      outline: none;
+      cursor: pointer;
+      font-size: 1.2rem;
+      color: #27ae60;
+      font-weight: 600;
+    }
+  }
+
+  .trailer-con {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    iframe {
+      outline: none;
+      border: 5px solid #e5e7eb;
+      padding: 1.5rem;
+      border-radius: 10px;
+      background-color: #fff;
+
+      background-color: #eeecf1;
+    }
+  }
+
+  .details {
+    background-color: #fff;
+    border-radius: 20px;
+    padding: 2rem;
+    border: 5px solid #e5e7eb;
+
+    .detail {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+
+      img {
+        border-radius: 7px;
+      }
+    }
+
+    .anime-details {
+      margin-left: 1rem;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      p {
+        display: flex;
+        gap: 1rem;
+      }
+
+      p span:first-child {
+        font-weight: 600;
+        color: #454e56;
+      }
+    }
+  }
+
+  .characters {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    grid-gap: 2rem;
+    background-color: #fff;
+    padding: 2rem;
+    border-radius: 20px;
+    border: 5px solid #e5e7eb;
+
+    .character {
+      padding: 0.4rem 0.6rem;
+      border-radius: 7px;
+      background-color: #ededed;
+      transition: all 0.4s ease-in-out;
+
+      img {
+        width: 100%;
+      }
+
+      h4 {
+        padding: 0.5rem 0;
+        color: #454e56;
+      }
+
+      p {
+        color: #27ae60;
+      }
+
+      &:hover {
+        transform: translateY(-5px);
+      }
+    }
+  }
+
+  h1 {
+    display: inline-block;
+    font-size: 3rem;
+    margin-bottom: 1.5rem;
+    cursor: pointer;
+    background: linear-gradient(to right, #a855f7, #27ae60);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
     transition: all 0.4s ease-in-out;
 
-    img {
-      width: 100%;
-    }
-
-    h4 {
-      padding: 0.5rem 0;
-      color: #454e56;
-    }
-
-    p {
-      color: #27ae60;
-    }
-
     &:hover {
-      transform: translateY(-5px);
+      transform: skew(-3deg);
     }
   }
-}
-
-h1 {
-  display: inline-block;
-  font-size: 3rem;
-  margin-bottom: 1.5rem;
-  cursor: pointer;
-  background: linear-gradient(to right, #a855f7, #27ae60);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  transition: all 0.4s ease-in-out;
-
-  &:hover {
-    transform: skew(-3deg);
-  }
-}
 `;
 export const GalleryStyled = styled.div`
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    .back{
-        position: absolute;
-        top: 2rem;
-        left: 2rem;
-        a{
-            font-weight: 600;
-            text-decoration: none;
-            color: #EB5757;
-            display: flex;
-            align-items: center;
-            gap: .5rem;
-        }
-    }
-    .big-image{
-        display: inline-block;
-        padding: 2rem;
-        margin: 2rem 0;
-        background-color: #fff;
-        border-radius: 7px;
-        border: 5px solid #e5e7eb;
-        position: relative;
-        img{
-            width: 350px;
-        }
-    }
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-    .small-images{
-        display: flex;
-        flex-wrap: wrap;
-        gap: .5rem;
-        width: 80%;
-        padding: 2rem;
-        border-radius: 7px;
-        background-color: #fff;
-        border: 5px solid #e5e7eb;
-        img{
-            width: 6rem;
-            height: 6rem;
-            object-fit: cover;
-            cursor: pointer;
-            border-radius: 5px;
-            border: 3px solid #e5e7eb;
-        } 
+  .back {
+    position: absolute;
+    top: 2rem;
+    left: 2rem;
+    a {
+      font-weight: 600;
+      text-decoration: none;
+      color: #eb5757;
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
     }
+  }
+  .big-image {
+    display: inline-block;
+    padding: 2rem;
+    margin: 2rem 0;
+    background-color: #fff;
+    border-radius: 7px;
+    border: 5px solid #e5e7eb;
+    position: relative;
+    img {
+      width: 350px;
+    }
+  }
+
+  .small-images {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+    width: 80%;
+    padding: 2rem;
+    border-radius: 7px;
+    background-color: #fff;
+    border: 5px solid #e5e7eb;
+    img {
+      width: 6rem;
+      height: 6rem;
+      object-fit: cover;
+      cursor: pointer;
+      border-radius: 5px;
+      border: 3px solid #e5e7eb;
+    }
+  }
 `;
--- src/styleglobal.js
+++ src/styleglobal.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap');
