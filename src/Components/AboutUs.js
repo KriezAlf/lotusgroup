79d77efc5a115dd013262479c0263e5838bbf304
KriezAlf <ignatius.steven03@gmail.com>
2023-05-30 11:10:19 +0700

    up

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,65 +1,88 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles.css";
-import Caroline from "./Image/Caroline.jpg";
-import Michael from "./Image/Michael.jpeg";
-import Steven from "./Image/Steven.jpg";
-import Reynata from "./Image/Reynata.jpg";
+import React from 'react';
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import { useGlobalContext } from '../context/global';
+import RightSidebar from './PopularSidebar';
+import LeftSidebar from './UpcomingSidebar';
+import { StyledDisplay, StyledCardLink } from '../style/style';
+import Caroline from "../Image/Caroline.jpg";
+import Michael from "../Image/Michael.jpeg";
+import Steven from "../Image/Steven.jpg";
+import Reynata from "../Image/Reynata.jpg";
 
-function CardComponent() {
+function AboutUs({ rendered }) {
+  const { airingAnime, isSearch, searchResults } = useGlobalContext();
+
+  const conditionalRender = () => {
+    if (!isSearch && rendered === 'aboutus') {
+      return (
+        <Flex justifyContent="center">
+          <Flex>
+            <Box borderWidth="1px" borderRadius="lg" overflow="hidden" m="4">
+              <Image src={Caroline} alt="Caroline" />
+              <Box p="4">
+                <Text fontSize="xl" fontWeight="bold">
+                  Caroline Susanto (00000071280)
+                </Text>
+                <Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Text>
+              </Box>
+            </Box>
+
+            <Box borderWidth="1px" borderRadius="lg" overflow="hidden" m="4">
+              <Image src={Steven} alt="Steven" />
+              <Box p="4">
+                <Text fontSize="xl" fontWeight="bold">
+                  Ignatius Steven (00000070642)
+                </Text>
+                <Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Text>
+              </Box>
+            </Box>
+          </Flex>
+
+          <Flex>
+            <Box borderWidth="1px" borderRadius="lg" overflow="hidden" m="4">
+              <Image src={Michael} alt="Michael" />
+              <Box p="4">
+                <Text fontSize="xl" fontWeight="bold">
+                  Michael Vallent (00000071361)
+                </Text>
+                <Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Text>
+              </Box>
+            </Box>
+
+            <Box borderWidth="1px" borderRadius="lg" overflow="hidden" m="4">
+              <Image src={Reynata} alt="Reynata" />
+              <Box p="4">
+                <Text fontSize="xl" fontWeight="bold">
+                  Reynata Prajnadi Tangajaya (00000070461)
+                </Text>
+                <Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Text>
+              </Box>
+            </Box>
+          </Flex>
+        </Flex>
+      );
+    } else {
+      return null;
+    }
+  }
+
   return (
-    <div>
-      <div className="row">
-        <div className="col">
-          <div className="card" style={{ width: "18rem" }}>
-            <img src={Caroline} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h1>Caroline Susanto (00000071280) </h1>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-          <div className="card" style={{ width: "18rem" }}>
-            <img src={Steven} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h1>Ignatius Steven (00000070642) </h1>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-        </div>
+    <StyledDisplay>
+      <LeftSidebar />
+      <div>
+        {conditionalRender()}
       </div>
-
-      <div className="row2">
-        <div className="col2">
-          <div className="card" style={{ width: "18rem" }}>
-            <img src={Michael} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h1>Michael Vallent (00000071361) </h1>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-          <div className="card" style={{ width: "18rem" }}>
-            <img src={Reynata} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h1>Reynata Prajnadi Tangajaya (00000070461)</h1>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      <RightSidebar />
+    </StyledDisplay>
   );
 }
 
-export default CardComponent;
+export default AboutUs;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,42 @@
+// Home.js
 import React from 'react';
 import { useGlobalContext } from '../context/global';
 import Popular from './Popular';
 import { Box, Button, Input, Flex } from '@chakra-ui/react';
 import Upcoming from './Upcoming';
 import Airing from './Airing';
+import AboutUs from './AboutUs';
+import LogoImage from '../Image/logo lotus.png';
+
+const Logo = () => {
+  const logoStyle = {
+    position: 'absolute',
+    width: '60px',
+    height: '49px',
+  };
+
+  return (
+    <div className="Gambar">
+      <img className="Logo" src={LogoImage} alt="Example" style={logoStyle} />
+    </div>
+  );
+};
+
+const MyComponent = () => {
+  return (
+    <div id="Utama">
+      <Logo />
+    </div>
+  );
+};
 
 function Home() {
   const {
     handleSubmit,
     search,
-    //SearchAnime,
     handleChange,
     getUpcomingAnime,
     getAiringAnime,
-    //getPopularAnime,
   } = useGlobalContext();
 
   const [rendered, setRendered] = React.useState('popular');
@@ -35,7 +58,6 @@ function Home() {
         );
     }
   };
-  
 
   const handleRenderedChange = (type) => {
     setRendered(type);
@@ -48,38 +70,114 @@ function Home() {
   };
 
   return (
-    <Box>
-      <Box py="2rem" px={{ base: '1rem', md: '5rem' }} maxW="60%" mx="auto" transition="all .4s ease-in-out">
+    <Box style={{ backgroundColor: 'Black' }}>
+      <MyComponent />
+      {/* Include the MyComponent here */}
+      <Box
+        py="2rem"
+        px={{ base: '1rem', md: '5rem' }}
+        maxW="60%"
+        mx="auto"
+        transition="all .4s ease-in-out"
+      >
         <Flex direction="column" align="center" mb="2rem">
-          <h1>{rendered === 'popular' ? 'Popular Anime' : rendered === 'airing' ? 'Airing Anime' : 'Upcoming Anime'}</h1>
+          <h1
+            style={{
+              position: 'absolute',
+              width: '261px',
+              height: '96px',
+              top: '15px',
+              fontFamily: 'Actor',
+              fontStyle: 'normal',
+              fontWeight: '200',
+              fontSize: '30px',
+              textAlign: 'center',
+              letterSpacing: '0.05em',
+              color: 'white',
+            }}
+          >
+            Our List
+          </h1>
         </Flex>
-        <Flex align="center" justify="center" gap="1rem">
-          <Button onClick={() => handleRenderedChange('popular')} variant="outline">
+        <Flex align="center" justify="center" gap="1rem" top="125px">
+          {/* Add the top property here */}
+          <Button
+            onClick={() => handleRenderedChange('popular')}
+            variant="outline"
+            style={{
+              color: 'white',
+            }}
+          >
             Popular
           </Button>
-          <Button onClick={() => handleRenderedChange('airing')} variant="outline">
+          <Button
+            onClick={() => handleRenderedChange('airing')}
+            variant="outline"
+            style={{
+              color: 'white',
+            }}
+          >
             Airing
           </Button>
-          <Button onClick={() => handleRenderedChange('upcoming')} variant="outline">
+          <Button
+            onClick={() => handleRenderedChange('upcoming')}
+            variant="outline"
+            style={{
+              color: 'white',
+            }}
+          >
             Upcoming
           </Button>
-          <Box as="form" onSubmit={handleSubmit}>
-            <Flex align="center">
-              <Input
-                type="text"
-                placeholder="Search Anime"
-                value={search}
-                onChange={handleChange}
-                borderRadius="30px"
-                border="5px solid #e5e7eb"
-                size="md"
-              />
-              <Button type="submit">Search</Button>
-            </Flex>
-          </Box>
+          <Button
+            onClick={() => handleRenderedChange('aboutus')}
+            variant="outline"
+            style={{
+              color: 'white',
+            }}
+          >
+            About Us
+          </Button>
         </Flex>
+        <Box as="form" onSubmit={handleSubmit}>
+          <Flex align="center">
+            <Input
+              type="text"
+              placeholder="Search Anime"
+              value={search}
+              onChange={handleChange}
+              style={{
+                position: 'absolute',
+                width: '418px',
+                height: '30px',
+                background: 'white',
+                mixBlendMode: 'normal',
+                borderRadius: '5px',
+                top: '110px',
+                left: '350px',
+              }}
+              size="md"
+            />
+            <Button
+              type="submit"
+              style={{
+                position: 'absolute',
+                borderRadius: '5px',
+                left: '800px',
+                top: '110px',
+
+                height: '30px',
+              }}
+            >
+              Search
+            </Button>
+          </Flex>
+        </Box>
       </Box>
-      {switchComponent()}
+      {rendered !== 'aboutus' ? (
+        switchComponent()
+      ) : (
+        <AboutUs rendered={true} />
+      )}
     </Box>
   );
 }
